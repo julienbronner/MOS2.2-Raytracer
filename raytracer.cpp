11870e6037b074d0d7938bab2f8926a6fe70a853
Julien Bronner <julbronner@free.fr>
2021-01-13 12:10:57 +0100

    ajout transparence

--- raytracer.cpp
+++ raytracer.cpp
@@ -42,6 +42,9 @@ Vector operator+(const Vector& a, const Vector& b) {
 Vector operator-(const Vector& a, const Vector& b) {
 	return Vector(a[0] - b[0], a[1] - b[1], a[2] - b[2]);
 }
+Vector operator-(const Vector& b) {
+	return Vector( -b[0], -b[1], -b[2]);
+}
 Vector operator*(double a, const Vector& b) {
 	return Vector(a * b[0], a * b[1], a * b[2]);
 }
@@ -50,6 +53,9 @@ Vector operator/(const Vector& b, double a) {
 }
 Vector operator*(const Vector& a, const Vector& b) {
 	return Vector(a[0] * b[0], a[1] * b[1], a[2] * b[2]);
+}
+double sqr(const double& a) {
+	return a * a;
 }
 double dot(const Vector& a, const Vector& b) {
 	return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
@@ -65,7 +71,7 @@ public:
 
 class Sphere {
 public:
-	Sphere(const Vector& O, double R, Vector rho, bool mirror) : O(O), R(R), rho(rho), mirror(mirror) {
+	Sphere(const Vector& O, double R, Vector rho, bool mirror, bool transparency, bool sphere_inverse) : O(O), R(R), rho(rho), mirror(mirror), transparency(transparency), sphere_inverse(sphere_inverse){
 	}
 	bool intersect(const Ray& r, Vector& P, Vector& N, double& t) {
 		// pour resoudre a*t^2 + b*t + c = 0
@@ -93,13 +99,16 @@ public:
 			}
 			P = r.C + t * r.u;
 			N = (P - O).get_normalized();
+			if (sphere_inverse) {
+				N = -N;
+			}
 			return true;
 		}
 	}
 	Vector O;
 	double R;
 	Vector rho;
-	bool mirror;
+	bool mirror, transparency, sphere_inverse;
 };
 
 class Light {
@@ -114,7 +123,7 @@ class Scene {
 public:
 	Scene(Light& Lum, Vector& color) : Lum(Lum), color(color) {
 	}
-	bool intersect_scene(const Ray& rayon, Vector& P, Vector& N, Vector& albedo, double& t, bool& mirror) {
+	bool intersect_scene(const Ray& rayon, Vector& P, Vector& N, Vector& albedo, double& t, bool& mirror, bool& transparency) {
 		//Vector P, N;
 		//double t = 2147483647;
 		//Vector couleur = color;
@@ -129,6 +138,7 @@ public:
 				t = t_current;
 				albedo = S.rho;
 				mirror = S.mirror;
+				transparency = S.transparency;
 				bool_sortie = true;
 				//Vector PL;
 				//PL = Lum.L - P;
@@ -139,13 +149,13 @@ public:
 	}
 	Vector getColor(Ray& rayon, const int& rebond) {
 
-		if (rebond > 5) return Vector(0., 0., 0.);
+		if (rebond > 10) return Vector(0., 0., 0.);
 
 		Vector coul = color;
 		Vector P, N, albedo;
 		double t = 2147483647;
-		bool mirror;
-		bool inter = intersect_scene(rayon, P, N, albedo, t, mirror);
+		bool mirror, transparency;
+		bool inter = intersect_scene(rayon, P, N, albedo, t, mirror, transparency);
 		if (inter) {
 			Vector PL;
 			PL = Lum.L - P;
@@ -156,20 +166,40 @@ public:
 				return getColor( rayon_reflechi, rebond + 1);
 			}
 			else {
-				Vector shadowP, shadowN, shadowAlbedo;
-				double shadowt = 2147483647;
-				bool shadowMirror;
-				Ray shadowRay(P + epsilon * PL.get_normalized(), PL.get_normalized());
-				bool shadowInter = intersect_scene(shadowRay, shadowP, shadowN, shadowAlbedo, shadowt, shadowMirror);
-				if (shadowInter && shadowt < sqrt(PL.sqrNorm())) {
-					coul = Vector(0., 0., 0.);
+				if (transparency) {
+					double n1 = 1, n2 = 1.4;
+					Vector N2 = N;
+					if (dot(rayon.u, N) > 0) {
+						std::swap(n1, n2);
+						N2 = -N;
+					}
+					Vector t_t = n1 / n2 * (rayon.u - dot(rayon.u, N2) * N2);
+					double radical = 1 - sqr(n1 / n2) * (1 - sqr(dot(rayon.u, N2)) );
+					if (radical < 0) {
+						Vector reflex = rayon.u - 2 * dot(rayon.u, N2) * N2;
+						Ray rayon_reflechi(P + epsilon * N2, reflex.get_normalized());
+						return getColor(rayon_reflechi, rebond + 1);
+					}
+					Vector t_n = -sqrt(radical) * N2;
+					Vector vect_dir_refracte = t_t + t_n;
+					Ray rayon_refracte(P - epsilon * N2, vect_dir_refracte);
+					return getColor(rayon_refracte, rebond + 1);
+
 				}
 				else {
-					coul = Lum.I / (4 * M_PI * PL.sqrNorm()) * std::max(0., dot(N, PL.get_normalized())) * albedo / M_PI;
+					Vector shadowP, shadowN, shadowAlbedo;
+					double shadowt = 2147483647;
+					bool shadowMirror, shadowTransparency;
+					Ray shadowRay(P + epsilon * PL.get_normalized(), PL.get_normalized());
+					bool shadowInter = intersect_scene(shadowRay, shadowP, shadowN, shadowAlbedo, shadowt, shadowMirror, shadowTransparency);
+					if (shadowInter && shadowt < sqrt(PL.sqrNorm())) {
+						coul = Vector(0., 0., 0.);
+					}
+					else {
+						coul = Lum.I / (4 * M_PI * PL.sqrNorm()) * std::max(0., dot(N, PL.get_normalized())) * albedo / M_PI;
+					}
 				}
 			}
-
-
 		}
 		return coul;
 	}
@@ -186,26 +216,25 @@ int main() {
 
 	Vector C(0, 0, 55);
 	int r = 10;
-	Sphere S1(Vector(0, 0, 0), r, Vector(1, 0, 0), true);
-	//Sphere S1(Vector(-15, 0, 0), r, Vector(1, 0, 0), true);
-	//Sphere S2(Vector(15, 0, 0), r, Vector(0, 1, 0), true);
-	//Sphere S3(Vector(0, 15, 0), r, Vector(0, 0, 1), false);
-	//Sphere S4(Vector(0, -15, 0), r, Vector(0, 1, 1), true);
-	Sphere SMurFace(Vector(0, 0, -1000), 940, Vector(0, 1, 0), false);
-	Sphere SMurDos(Vector(0, 0, 1000), 940, Vector(1, 0, 1), false);
-	Sphere SMurHaut(Vector(0, 1000, 0), 940, Vector(1, 0, 0), false);
-	Sphere SMurBas(Vector(0, -1000, 0), 990, Vector(0, 0, 1), false);
-	Sphere SMurDroite(Vector(1000, 0, 0), 940, Vector(0, 1, 1), false);
-	Sphere SMurGauche(Vector(-1000, 0, 0), 940, Vector(1, 1, 0), false);
+	Sphere S1(Vector(-20, 0, 0), r, Vector(1, 0, 0), true, false, false);
+	Sphere S2(Vector(0, 0, 0), r, Vector(1, 0, 0), false, true, false);
+	Sphere S3(Vector(20, 0, 0), r, Vector(0, 0, 1), false, true, false);
+	Sphere S4(Vector(20, -0, 0), 9.5, Vector(0, 1, 1), false, true, true);
+	Sphere SMurFace(Vector(0, 0, -1000), 940, Vector(0, 1, 0), false, false, false);
+	Sphere SMurDos(Vector(0, 0, 1000), 940, Vector(1, 0, 1), false, false, false);
+	Sphere SMurHaut(Vector(0, 1000, 0), 940, Vector(1, 0, 0), false, false, false);
+	Sphere SMurBas(Vector(0, -1000, 0), 990, Vector(0, 0, 1), false, false, false);
+	Sphere SMurDroite(Vector(1000, 0, 0), 940, Vector(0, 1, 1), false, false, false);
+	Sphere SMurGauche(Vector(-1000, 0, 0), 940, Vector(1, 1, 0), false, false, false);
 
 	double fov = 60 * M_PI / 180;
 	Light Lum(double(4E9), Vector(-10, 20, 40));
 	Vector couleur(0, 0, 0);
 	Scene scene(Lum, couleur);
 	scene.objects.push_back(S1);
-	//scene.objects.push_back(S2);
-	//scene.objects.push_back(S3);
-	//scene.objects.push_back(S4);
+	scene.objects.push_back(S2);
+	scene.objects.push_back(S3);
+	scene.objects.push_back(S4);
 	double gamma = 0.45;
 	
 	scene.objects.push_back(SMurFace);
